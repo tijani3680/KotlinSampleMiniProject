5142bd01c8bf7e3a33ae90815beb0df3cd65ed51
Tijani3680 <tijanima.71@gmail.com>
2022-07-01 15:00:57 +0430

    refactor: some edited

--- app/src/main/java/com/tijani/rememberkotlinskills/ui/view/fragment/FragmentHome.kt
+++ app/src/main/java/com/tijani/rememberkotlinskills/ui/view/fragment/FragmentHome.kt
@@ -24,7 +24,6 @@ class FragmentHome : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     private val fakeUserVM: FakeUserVM by viewModel()
     private lateinit var userAdapter: AdapterUsers
-    private var name =""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
